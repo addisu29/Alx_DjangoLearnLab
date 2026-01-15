2372d5bfdf37a5cbd3daaad38d45e390d5e41ef7
addisu29 <addisum2129@gmail.com>
2026-01-15 01:00:22 -0800

    Implement custom permissions for Book model and secure views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,12 +4,15 @@ from . import views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
     path('login/', views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
     
-    # Role-based URLs
-    path('admin_view/', views.admin_view, name='admin_view'),
-    path('librarian_view/', views.librarian_view, name='librarian_view'),
-    path('member_view/', views.member_view, name='member_view'),
+    # New Permission-based URLs
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
