6874f8e0d7079d435c5da8e2a73cb0f182094746
addisu29 <addisum2129@gmail.com>
2026-01-29 04:27:02 -0800

    Implement full CRUD functionality using BookViewSet and DefaultRouter

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,14 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
 
+# Your existing List View
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# New ViewSet for all CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
