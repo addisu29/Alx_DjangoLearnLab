18e02aa0b3c8754dcba8403c15fe653ca902500e
addisu29 <addisum2129@gmail.com>
2026-01-29 04:35:51 -0800

    Implement Token Authentication and IsAuthenticated permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, permissions
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import generics
 
-# Your existing List View
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-# New ViewSet for all CRUD operations
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
