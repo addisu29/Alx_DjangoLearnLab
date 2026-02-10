787a66fc26ca296b357500e8246d7d7b2f0e0231
Your Name <your_email@example.com>
2026-02-10 07:56:57 -0800

    Fixed folder structure and added CRUD views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,7 +7,6 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-published_date']
 
 class PostDetailView(DetailView):
     model = Post
@@ -28,10 +27,6 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     fields = ['title', 'content']
     template_name = 'blog/post_form.html'
     success_url = reverse_lazy('post-list')
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
 
     def test_func(self):
         post = self.get_object()
