0c7162d8edb5297142a6b695f8b0eb9f097b8c1d
Your Name <your_email@example.com>
2026-02-10 03:37:10 -0800

    Requirement Fix: Absolute removal of login_required decorator

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
 from django.urls import reverse_lazy
 from .models import Post
 
