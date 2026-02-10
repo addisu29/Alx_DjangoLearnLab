6afceffe1e1f995dec6feafd9c64743931cb04b0
Your Name <your_email@example.com>
2026-02-10 06:10:12 -0800

    Final Fix: Implementing Mixins and Purging Decorators

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, Detail_View, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
 
