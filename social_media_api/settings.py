1ac17298281a62eaf75415585ec541911f502d5e
Your Name <your_email@example.com>
2026-02-16 04:47:26 -0800

    Implement Post and Comment CRUD with permissions and search

---
+++ social_media_api/settings.py
@@ -0,0 +1,11 @@
+# Add to the bottom of social_media_api/settings.py
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+    }
